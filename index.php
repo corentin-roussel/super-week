fc7938fa82c0edc0e61a40a30c9535e14d0d9927
corentin-roussel <corentin.roussel@laplateforme.io>
2023-05-02 11:00:34 +0200

    implement composer autoloader

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
     require_once ("vendor/autoload.php");
+
+    $router = new AltoRouter();
+
+    $router->setBasePath('/super-week');
+    $router->map( 'GET', '/', function(){
+        echo "Hello World";
+    } , 'index');
+
+    $match = $router->match();
+
+    if( is_array($match) && is_callable( $match['target'] ) ) {
+        call_user_func_array( $match['target'], $match['params'] );
+    } else {
+        // no route was matched
+        header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    }
